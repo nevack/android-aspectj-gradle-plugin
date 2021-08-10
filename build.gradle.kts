07896ade10d2d0be0d087e1bf3d9c77ce1f55bde
Dzmitry Neviadomski <nevack@yandex-team.ru>
2021-08-10 23:45:20 +0300

    [main] Update build files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,18 @@
 buildscript {
     repositories {
-        jcenter()
+        google()
         mavenCentral()
-    }
-
-    val kotlinVersion: String by extra
-    dependencies {
-        classpath(kotlin("gradle-plugin", kotlinVersion))
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.7.3")
     }
 }
 
 allprojects {
     repositories {
         google()
-        jcenter()
         mavenCentral()
-        maven { url = uri("http://repository.jetbrains.com/utils") }
-        maven { url = uri("https://dl.bintray.com/archinamon/maven") }
     }
+}
+
+tasks.wrapper {
+    version = "7.0.2"
+    distributionType = Wrapper.DistributionType.ALL
 }
